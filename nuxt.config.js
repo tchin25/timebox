ee3c31f69893ea09e8baab7ccffc714b47d557bd
Thomas Chin <tchin25@gmail.com>
2020-09-09 23:56:29 -0400

    deleted unnecessary items

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
-    { src: '@/plugins/draggable'} 
+    { src: '@/plugins/vuedraggable'} 
   ],
   /*
   ** Auto import components
