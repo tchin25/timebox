a5680e4ccc996ff2e1dd917d980d97c090603362
Thomas Chin <tchin25@gmail.com>
2020-09-14 09:35:55 -0400

    began writing test

---
+++ test/store/timebox.spec.js
@@ -0,0 +1,27 @@
+import { createLocalVue } from "@vue/test-utils";
+import { getters } from "../../store/timebox";
+
+describe("timebox/getters", () => {
+  const state = {
+    timeboxList: [
+      {
+        id: 25
+      },
+      {
+        id: 20
+      },
+      {
+        id: 5
+      }
+    ]
+  };
+  const getTimeboxIndexById = getters.getTimeboxIndexById(state);
+  test("Successfully finds index from id", () => {
+    expect(getTimeboxIndexById(25)).toBe(0);
+    expect(getTimeboxIndexById(20)).toBe(1);
+    expect(getTimeboxIndexById(5)).toBe(2);
+  });
+  test("Returns -1 when id does not exist", () => {
+    expect(getTimeboxIndexById(30)).toBe(-1);
+  });
+});
