d69d52fc359d9e79a65af89a017daca6a03dd527
Thomas Chin <tchin25@gmail.com>
2020-09-15 20:08:35 -0400

    100% coverage on DurationPicker

--- test/components/DurationPicker.spec.js
+++ test/components/DurationPicker.spec.js
@@ -19,7 +19,7 @@ describe("DurationPicker.vue", () => {
     ).toBe("00:01:00");
   });
 
-  test("Change emits input event", async () => {
+  test("Change emits input event with duration in seconds", async () => {
     // Need data-app element for Vuetify
     let app = document.createElement("div");
     app.setAttribute("data-app", true);
@@ -28,8 +28,6 @@ describe("DurationPicker.vue", () => {
     const vuetify = new Vuetify({});
     const localVue = createLocalVue();
 
-    const mockSet = jest.fn();
-
     const wrapper = mount(DurationPicker, {
       propsData: {
         value: 60
@@ -37,20 +35,14 @@ describe("DurationPicker.vue", () => {
       data() {
         return { timePicker: true };
       },
-      computed: {
-        duration: {
-          get: function(){},
-          set: function(newVal){mockSet(newVal)}
-        }
-      },
       localVue,
       vuetify
     });
-    
+
     let menu = wrapper.findComponent({ name: "v-time-picker" });
     menu.vm.$emit("input", "00:01:30");
     await wrapper.vm.$nextTick();
-    expect(mockSet.mock.calls.length).toBe(1);
-    expect(mockSet.mock.calls[0][0]).toBe("00:01:30");
+    expect(wrapper.emitted().input).toBeTruthy();
+    expect(wrapper.emitted().input[0]).toEqual([90]);
   });
 });
